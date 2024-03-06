bf39b6b68befd3ef62c5a6f29f85a9a2506e5434
Jimmy Ding <jimmyguding@gmail.com>
2024-03-07 02:49:04 +0800

    feat: updated tailwind

--- app/config/config.exs
+++ app/config/config.exs
@@ -42,7 +42,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.4.0",
+  version: "3.4.1",
   app: [
     args: ~w(
       --config=tailwind.config.js
